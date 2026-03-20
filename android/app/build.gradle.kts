1ecb668a6dda928da26fb996b013261f324f1835
Montathr Alkateb <montathralkateb600@gmail.com>
2026-03-20 21:56:32 +0300

    Final Fix: Normalized Bundle ID and Standalone Podfile

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.most_beautiful"
+    namespace = "com.mostbeautiful.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.most_beautiful"
+        applicationId = "com.mostbeautiful.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/most_beautiful/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/most_beautiful/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.most_beautiful
+package com.mostbeautiful.app
 
 import io.flutter.embedding.android.FlutterActivity
 
